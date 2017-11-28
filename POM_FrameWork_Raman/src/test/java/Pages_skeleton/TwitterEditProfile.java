6401ce35bd4da93d87c436279e00d928f6bd6270
Sami Sabir <samisabir212@gmail.com>
2017-11-28 15:26:48 -0500

    Working on understanding geico excel data

--- POM_FrameWork_Raman/src/test/java/Pages_skeleton/TwitterEditProfile.java
+++ POM_FrameWork_Raman/src/test/java/Pages_skeleton/TwitterEditProfile.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.PageFactory;
 public class TwitterEditProfile extends TwitterMyProfile {
 
 
-    @FindBy(how = by.xpath, )
+    @FindBy
     WebElement addProfilePhotoButton;
 
     @FindBy
